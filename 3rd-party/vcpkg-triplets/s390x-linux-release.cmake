5621885fa0b791c69d669e57c20d5f4bd83d7c7a
Scott Harder <scott.harder@canonical.com>
2026-07-31 08:40:42 -0500

    [vcpkg] Disable valgrind on s390x

--- 3rd-party/vcpkg-triplets/s390x-linux-release.cmake
+++ 3rd-party/vcpkg-triplets/s390x-linux-release.cmake
@@ -7,3 +7,8 @@ set(VCPKG_BUILD_TYPE release)
 # Workaround for dbus and qtbase: when vcpkg sets VCPKG_CMAKE_SYSTEM_NAME, CMake may treat
 # native s390x builds as cross-compiling.
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_CROSSCOMPILING:BOOL=OFF")
+
+# Valgrind does not compile without errors with newer version of clang. Valgrind is compiled
+# as a transitive dependency of glib and QEMU. Note that this will disable Valgrind whenever
+# its linked by meson.
+list(APPEND VCPKG_MESON_CONFIGURE_OPTIONS "-Dvalgrind=disabled")
